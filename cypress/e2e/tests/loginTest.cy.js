0575739d12eb2d410630203ffb67ab0abc2586dc
Adam Dumanski <adam.dumanski@gmail.com>
2026-02-27 08:30:00 +0100

    updated login tests - test for locked user

--- cypress/e2e/tests/loginTest.cy.js
+++ cypress/e2e/tests/loginTest.cy.js
@@ -5,21 +5,24 @@ describe('Login Tests' , () => {
         loginPage.visit()
     })
 
-    it('Powinien zalogować użytkownika z poprawnymi danymi', () => {
+    it('Test 1: Powinien zalogować użytkownika z poprawnymi danymi', () => {
         loginPage.login('standard_user', 'secret_sauce')
 
         cy.url().should('contain' , 'inventory')
         cy.get('[data-test="title"]').should('have.text' , 'Products')
     })
-    it('Blad przy podaniu zlego hasla', () => {
+    it('Test 2: Blad przy podaniu zlego hasla', () => {
         loginPage.login('standard_user' , 'wrong_pass')
         
         loginPage.shouldShowError('Username and password do not match')
     })
-    it('Blad przy pustych danych' , () => {
+    it('Test 3: Blad przy pustych danych' , () => {
         loginPage.loginButton.click()
 
         loginPage.shouldShowError('Username is required')
     })
-
+    it('Test 4: "Powinien pokazać błąd dla zablokowanego użytkownika"', () => {
+        loginPage.login('locked_out_user' , 'secret_sauce')
+        loginPage.shouldShowError('Sorry, this user has been locked out')
+    })
 })
